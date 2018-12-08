48133466232b426c7a763fc1a617429b1109c319
emousavi97 <emousavi97@gmail.com>
2018-12-08 23:19:31 +0330

    قابل بازی

---
+++ Assets/player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+	Rigidbody2D rb;
+	Vector2 dir;
+	public float makan=0;
+	public float force=1;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+	void FixedUpdate (){
+
+		Vector3 worldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Vector2 worldPoint2d = new Vector2 (worldPoint.x, worldPoint.y);
+
+		dir = new Vector2 (transform.position.x, transform.position.y) - worldPoint2d;
+		if (Input.GetMouseButton (0)) {
+			rb.AddForceAtPosition (dir.normalized * force, new Vector2 (transform.position.x, transform.position.y));
+		}
+	}
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.CompareTag ("ndl")) {
+			Destroy (gameObject);
+		}
+	}
+}
